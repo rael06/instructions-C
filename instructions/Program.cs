d0c08f25bb843410f05a37d8d9cb698446c09f1d
rael <rael06@hotmail.fr>
2019-08-16 19:23:11 +0200

    if-else done

--- instructions/Program.cs
+++ instructions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace instructions
 {
@@ -6,7 +7,22 @@ namespace instructions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+//            int limit = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine(limit);
+//            int min = 0;
+//            int max = 0;
+//            for (int i = 0; i < limit; i++)
+//            {
+//                Random random = new Random();
+//                int j = random.Next(1, 10);
+//                if (j <= 5) min++;
+//                if (j > 5) max++;
+//            }
+//
+//            Console.WriteLine("Proportions min et max : " + ((double) min) / limit * 100 + " " +
+//                              ((double) max) / limit * 100);
+            char a = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(char.IsLetter(a) ? "vrai" : "faux");
         }
     }
 }

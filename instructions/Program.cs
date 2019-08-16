1399ef007d510e3d9258b71ba68b914d60f2fcd2
rael <rael06@hotmail.fr>
2019-08-16 18:48:59 +0200

    Initial commit

---
+++ instructions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace instructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
